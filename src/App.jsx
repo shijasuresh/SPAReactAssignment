a872cc61e89f01c35790c1a3c6d14f0c641a578d
shijasuresh <shija.suresh89@gmail.com>
2024-09-21 10:17:34 +1000

    Changed the Home Page to Requests Page

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Requests />} />
         <Route path="/about" element={<About />} />
         <Route path="/requests" element={<Requests />} />
         <Route path="/:username" element={<UserDetails />} />
